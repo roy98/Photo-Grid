e0fea650183434af7b625f8b917f6fb5d4127f0d
Roy Christo <christoroybiaga@gmail.com>
2023-08-11 17:34:18 -0400

    add photo schema for mongo

--- photo-server/models/PhotoModel.js
+++ photo-server/models/PhotoModel.js
@@ -2,6 +2,16 @@ var mongoose = require('mongoose')
 
 var Schema = mongoose.Schema
 
-var PhotoSchema = new Schema({}, { timestamps: true })
+var PhotoSchema = new Schema(
+	{
+		id: { type: String, required: true },
+		description: { type: String, required: false },
+		urls: { type: String, required: true },
+		downloads: { type: Number, required: false },
+		likes: { type: Number, required: false },
+		user: { type: String, required: true }
+	},
+	{ timestamps: true }
+)
 
 module.exports = mongoose.model('Photo', PhotoSchema)
