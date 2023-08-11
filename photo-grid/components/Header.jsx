d478bd325822839a14b1c09946b91fc0b1ca0bbb
Roy Christo <christoroybiaga@gmail.com>
2023-08-11 17:34:17 -0400

    customize the header component

--- photo-grid/components/Header.jsx
+++ photo-grid/components/Header.jsx
@@ -1,9 +1,23 @@
 import styles from '@/styles/common.module.css'
+import Image from 'next/image'
 
 export default function Header() {
 	return (
-		<header className={styles.header}>
-			<span>Photo Grid</span>
-		</header>
+		<>
+			<header className={styles.header}>
+				<div className={styles.logoContainer}>
+					<span className={styles.appName}>Photo Grid</span>
+					<span className={styles.logo}>
+						<Image src='/images/logo.svg' alt='Vercel Logo' width={50} height={50} />
+					</span>
+				</div>
+				<p>
+					Powered by <a href=''>Unsplash</a>
+				</p>
+			</header>
+			<p style={{ padding: '3rem 2rem 2rem 2rem', textAlign: 'center' }}>
+				The most beautiful royalty-free photos and images shared for free by talented creators.
+			</p>
+		</>
 	)
 }
