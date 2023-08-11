26a2986bb1c97451795c49e2dddffa03c246962b
Roy Christo <christoroybiaga@gmail.com>
2023-08-11 17:34:13 -0400

    install and use react infinite scroll component

--- photo-grid/pages/index.tsx
+++ photo-grid/pages/index.tsx
@@ -1,12 +1,12 @@
-import { useEffect, useState } from 'react'
 import useSWRInfinite from 'swr/infinite'
 import { Key, Fetcher } from 'swr'
 import Head from 'next/head'
-import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import { getPhotoFromUnsplash } from '../utils/method'
 import SingleImage from '../components/SingleImage'
-import { Bars } from 'react-loader-spinner'
+import Loader from '../components/Loader'
+import Header from '../components/Header'
+import InfiniteScroll from 'react-infinite-scroll-component'
 
 const getKey = (pageIndex, previousPageData) => {
 	if (previousPageData && !previousPageData.length) return null // reached the end
@@ -19,16 +19,7 @@ export default function Home() {
 	const { data, error, isValidating, size, setSize } = useSWRInfinite(getKey, fetcher)
 	const images: Array<any> = data ? [].concat(...data) : []
 
-	const isLoadingInitialData = true || (!data && !error)
-	const isLoadingMore = size > 0 && data && typeof data[size - 1] === 'undefined'
-
-	if (isLoadingInitialData) {
-		return (
-			<div className={styles.loaderContainer}>
-				<Bars height='80' width='80' visible color='#640aa1' ariaLabel='Loader' />
-			</div>
-		)
-	}
+	const isLoadingInitialData = !data && !error
 
 	return (
 		<div className={styles.container}>
@@ -38,23 +29,21 @@ export default function Home() {
 				<link rel='icon' href='/favicon.ico' />
 			</Head>
 
-			<main className={styles.main}>
-				{images &&
-					images.map((image: any) => <SingleImage url={image.urls?.regular} key={image?.id} />)}
-			</main>
+			<Header />
 
-			<footer className={styles.footer}>
-				<a
-					href='https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app'
-					target='_blank'
-					rel='noopener noreferrer'
-				>
-					Powered by{' '}
-					<span className={styles.logo}>
-						<Image src='/vercel.svg' alt='Vercel Logo' width={72} height={16} />
-					</span>
-				</a>
-			</footer>
+			{isLoadingInitialData && <Loader />}
+
+			<InfiniteScroll
+				dataLength={images.length}
+				next={() => setSize(size + 1)}
+				hasMore
+				loader={<Loader />}
+			>
+				<main className={styles.main}>
+					{images &&
+						images.map((image: any) => <SingleImage url={image.urls?.regular} key={image?.id} />)}
+				</main>
+			</InfiniteScroll>
 		</div>
 	)
 }
